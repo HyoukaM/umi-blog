7f33edb6cbe42c5f895033899e0820a9dd90dc00
hyoukam <838611931@qq.com>
2022-03-21 09:30:34 +0800

    feat: update

--- src/components/card-info/CardInfo.tsx
+++ src/components/card-info/CardInfo.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import cardInfo from '@/style/components/card-info.less';
 import InfoAboutMe from '@/components/card-info/InfoAboutMe';
 import InfoCategory from '@/components/card-info/InfoCategory';
 
 const CardInfo = () => {
   return (
-    <div className={cardInfo.cardInfo}>
+    <>
       <InfoAboutMe />
       <InfoCategory />
-    </div>
+    </>
   );
 };
 
--- src/components/card-info/InfoCategory.tsx
+++ src/components/card-info/InfoCategory.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import cardInfo from '@/style/components/card-info.less';
+import query from '@/cloudbase-api/query';
+import { CategoryType } from '@/cloudbase-api/blogInterface';
 
 const InfoCategory = () => {
-  return <div>123</div>;
+  const [category, setCategory] = useState<CategoryType[]>([]);
+  /**
+   * 获取全部标签
+   */
+  const getCategory = () => {
+    query('categories', {}).then((res) => {
+      setCategory(res);
+    });
+  };
+  useEffect(() => {
+    getCategory();
+  }, []);
+  return (
+    <div className={cardInfo.infoCategory}>
+      <div className={cardInfo.infoCategoryList}>
+        {category.map((i) => {
+          return <span key={i._id}>{i.title}</span>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default InfoCategory;
