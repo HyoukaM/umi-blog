1ea666803ef4a879deac24fa4bfc62576afd8a1c
hyoukam <838611931@qq.com>
2022-03-23 14:27:08 +0800

    feat(info-category): 添加分类跳转链接

--- src/components/card-info/InfoCategory.tsx
+++ src/components/card-info/InfoCategory.tsx
@@ -2,16 +2,26 @@ import React, { useContext, useEffect, useState } from 'react';
 import cardInfo from '@/style/components/card-info.less';
 import { CategoryType } from '@/cloudbase-api/blogInterface';
 import LayoutContext from '@/context/layoutContext';
+import { useHistory } from 'umi';
 
 const InfoCategory = () => {
   const [category, setCategory] = useState<CategoryType[]>([]);
   const { categorys } = useContext(LayoutContext);
+  const history = useHistory();
   /**
    * 获取全部标签
    */
-  let getCategory = () => {
+  const getCategory = () => {
     setCategory(categorys);
   };
+  /**
+   * 跳转链接
+   * @param id
+   */
+  const linkCategory = (id: string) => {
+    history.push(`category?_id=${id}`);
+  };
+
   useEffect(() => {
     getCategory();
   }, [categorys]);
@@ -19,7 +29,11 @@ const InfoCategory = () => {
     <div className={cardInfo.infoCategory}>
       <div className={cardInfo.infoCategoryList}>
         {category.map((i) => {
-          return <span key={i._id}>{i.title}</span>;
+          return (
+            <span onClick={() => linkCategory(i._id)} key={i._id}>
+              {i.title}
+            </span>
+          );
         })}
       </div>
     </div>
